cefa33c70e92bfb3c4f7ca488d659bd1c5d15ff4
Bhawani1200 <bhawanilimbu1200@gmail.com>
2023-12-17 21:22:25 +0545

    new file

---
+++ swasthya/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LogoDemo from './Components/LogoDemo';
+const App = () => {
+  return (
+    <React.Fragment>
+   <LogoDemo/>
+   </React.Fragment>
+  )
+}
+
+export default App;
